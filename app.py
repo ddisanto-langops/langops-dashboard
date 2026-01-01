53c3e2f702111fff6aafbba14332fb77b44f4b30
ddisanto-langops <1daniel.disanto@gmail.com>
2026-01-01 17:05:20 -0500

    Added missing database init

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ init_db()
 @app.route('/langops-dashboard/products', methods=['GET'])
 def get_products():
     logger.info("Fetching products...")
+    init_db()
     session = get_db_session()
     try:
         products = session.query(TranslationProduct).all()
