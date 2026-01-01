001c75b0c4d31e26ea6002687ed6aeec0b191019
ddisanto-langops <1daniel.disanto@gmail.com>
2026-01-01 12:25:26 -0500

    Added Google Apps Script function to fetch data from server

---
+++ apps_script_fetch_products.js
@@ -0,0 +1,40 @@
+function fetch_products() {
+  
+  const server_url = "langops-ca.com/langops-dashboard/products"
+  try {
+    const response = UrlFetchApp.fetch(server_url);
+    const data_array = JSON.parse(response)
+    const values = data_array.map(item => [
+      item.title,
+      item.target_language,
+      item.due_by,
+      item.last_activity,
+      item.published,
+      item.progress.translation,
+      item.progress.approval
+      ]);
+
+    const headers = [[
+      "Title",
+      "Target Language",
+      "Due",
+      "Last Activity",
+      "Published",
+      "Translation Progress",
+      "Approval Progress"
+    ]];
+
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getSheetByName("Tracker");
+    
+    const headersRange = sheet.getRange(23,1, 1, headers[0].length);
+    headersRange.setValues(headers);
+
+    const dataRange = sheet.getRange(24,1,values.length, headers[0].length);
+    dataRange.setValues(values);
+    
+  }
+  catch (error) {
+    console.log(`Error fetching products: ${error}`);
+  }
+}
