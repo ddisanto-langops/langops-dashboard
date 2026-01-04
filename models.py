7f26ba2718791ecaf20d4daa04fde060b269c45f
ddisanto-langops <1daniel.disanto@gmail.com>
2026-01-03 21:05:50 -0500

    Add handling for missing due dates

--- models.py
+++ models.py
@@ -96,33 +96,34 @@ class TranslationProduct(Base):
 		Crowdin information using the class methods above.
 		Outputs can be 'To-Do', 'In Progress' or 'Completed'.
 		"""
-		
-		# parse last activity and due date into UTC datetime objects
-		last_activity_utc = datetime.fromisoformat(self.trello_last_activity)
-		due_utc = datetime.fromisoformat(self.trello_due)
-		
-		# convert to central time
-		central_time = ZoneInfo('America/Chicago')
-		last_activity_ct = last_activity_utc.astimezone(central_time)
-		due_ct = due_utc.astimezone(central_time)
-		
-		# compare converted datetime to current central time
+		# current central time
 		now_ct = datetime.now(central_time)
-		
-		# create a duration of desired comparison length
-		seven_days = timedelta(days=7)
-		
-		# Was last activity more than 7 days ago?
-		if now_ct - last_activity_ct > seven_days:
-			older_than_7_days = True
-		else:
-			older_than_7_days = False
+
+		# parse last activity and due date into UTC datetime objects
+		if self.trello_last_activity:
+			last_activity_utc = datetime.fromisoformat(self.trello_last_activity)
+			
+			# convert to central time
+			central_time = ZoneInfo('America/Chicago')
+			last_activity_ct = last_activity_utc.astimezone(central_time)
+			
+			# create a duration of desired comparison length
+			seven_days = timedelta(days=7)
+			
+			# Was last activity more than 7 days ago?
+			if now_ct - last_activity_ct > seven_days:
+				older_than_7_days = True
+			else:
+				older_than_7_days = False
 		
 		# is the product overdue?
-		if now_ct > due_ct:
-			overdue = True
-		else:
-			overdue = False
+		if self.trello_due:
+			due_utc = datetime.fromisoformat(self.trello_due)
+			due_ct = due_utc.astimezone(central_time)
+			if now_ct > due_ct:
+				overdue = True
+			else:
+				overdue = False
 		
 		# Main comparison cases
 		# Past due date and not published: OVERDUE
