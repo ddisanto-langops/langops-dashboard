ae4a652d81c3e22de5defa9279c834d3808cc157
ddisanto-langops <1daniel.disanto@gmail.com>
2026-01-06 01:44:14 -0500

    Improve cryptography

--- wrappers.py
+++ wrappers.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import secrets
 from functools import wraps
 from flask import request, jsonify
 
@@ -8,17 +9,18 @@ logger = logging.getLogger(__name__)
 def require_x_auth(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # 1. Grab the key from the 'X-Auth' header
         provided_key = request.headers.get('X-Auth')
         secret_key = os.environ.get('X_AUTH')
 
-        # 2. Validate
-        if provided_key and provided_key == secret_key:
+        if not secret_key:
+            return jsonify({'error':'Server configuration error'}), 500
+        
+        if provided_key and secrets.compare_digest(provided_key, secret_key):
             logger.info(f"X-Auth validated for path: {request.path}")
             return f(*args, **kwargs)
         
         # 3. Handle Failure
-        logger.critical(f"Invalid or missing X-Auth key for path: {request.path}")
+        logger.critical(f"Invalid or missing X-Auth key")
         return jsonify({"error": "Unauthorized"}), 401
         
     return decorated_function
